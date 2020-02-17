b85c595694ea95e5d6ffc7dbe6e3695b03a20e1f
cnahuina <cnahuina@gmail.com>
2020-02-17 10:02:17 -0500

    upload files spa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+// Rutas
+import { APP_ROUTING } from './app.routes';
+
+// Servicios
+import { HeroesService } from './servicios/heroes.service';
+
+
+// componentes
 import { AppComponent } from './app.component';
+import { NabvarComponent } from './components/shared/nabvar/nabvar.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
+import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NabvarComponent,
+    HomeComponent,
+    HeroesComponent,
+    AboutComponent,
+    HeroeComponent,
+    BuscadorComponent,
+    HeroeTarjetaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/shared/nabvar/nabvar.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-nabvar',
+  templateUrl: './nabvar.component.html'
+})
+export class NabvarComponent implements OnInit {
+
+  constructor( private router:Router) { }
+
+  ngOnInit() {
+
+  }
+
+  buscarHeroe(termino: string){
+    // console.log(termino);
+
+    this.router.navigate(['/buscar',termino]);
+  }
+}
